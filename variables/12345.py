61a195cea7c21fa990f33670f35cc26e682aac91
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 23:10:20 +0500

    variables in progress

---
+++ variables/12345.py
@@ -0,0 +1,28 @@
+#1
+a = 5
+b = "Bella"
+print(a)
+print(b)
+#2
+ab = "Bella"
+a_b_ = "Bella"
+a_b = "Bella"
+AB = "Bella"
+print(ab)
+print(a_b_)
+print(a_b)
+print(AB)
+#3
+a, b, c= "apple", "banana", "cherry"
+print(a)
+print(b)
+print(c)
+#4
+a="Hello"
+b="World"
+print(f"{a}, {b}")
+#5
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+myfunc()
