ef2a1052b31849ad98358a39fae62e4edd5322a6
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 23:24:49 +0500

    numbers in progress

---
+++ numbers/12345.py
@@ -0,0 +1,35 @@
+#1
+x = 1 
+y = 2.8  
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+#2
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+#3
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+#4
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+#5
+x = -122423453
+y = 1039.348948
+z = 37675e3
+print(type(x))
+print(type(y))
+print(type(z))
