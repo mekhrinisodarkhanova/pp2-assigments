ae94d19adcedb882ab67c2e1a84978d3571617d3
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 22:43:21 +0500

    syntax_syntax in progress

---
+++ syntax_syntax/12345.py
@@ -0,0 +1,22 @@
+#1
+a = 4
+if a < 5:
+    print("a less then 5")
+#2
+a = 5
+b = 3
+if a > b:
+    print("a greater then b")
+#3
+a = 0
+if a % 2 == 0:
+    print(a)
+#4
+a=2
+b=4
+if (b%2==0) and (a%2==0):
+    print("both of then are even")
+#5
+name="mekhriniso"
+if name.islower():
+    print(f"hi, {name}")
