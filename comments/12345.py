6c349b348cb6751347ca5c6020950ba5aa1ceaa7
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 22:53:47 +0500

    comments in progress

---
+++ comments/12345.py
@@ -0,0 +1,18 @@
+#1
+#this is a comment
+print("hello world")
+#2
+#print("a+b")
+print("hi kbtu")
+#3
+a=2
+print(a) #comment
+#4
+#you are the best professor, Nikita!)
+a=2
+b=4
+print(a+b)
+#5
+#Input your name
+name=input()
+print(f"hi, {name}")
