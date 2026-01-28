6b0b227248fdf23c02a8d54f86c34e395335a89e
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 23:33:46 +0500

    string in progress

---
+++ string/12345.py
@@ -0,0 +1,22 @@
+#1
+print("hello world")
+print('hello world')
+#2
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#3
+a = "Hi"
+print(a)
+#4
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#5
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
