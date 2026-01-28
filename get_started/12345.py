8b5a79b77e1e244d245789137bcbe87020cd9639
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 22:36:33 +0500

    get_started in progress

---
+++ get_started/12345.py
@@ -0,0 +1,15 @@
+#1
+print("hello, students!")
+#2
+name=input()
+print(f"hi, {name}")
+#3
+a=345
+print(a)
+#4
+a=5
+b=34
+print(a*b)
+#5
+surname="darkhanova"
+print(f"Your surname is {surname}, yes?")
