fd83a6187d418790e7bd89f7e47397320e2a05fc
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-28 22:48:30 +0500

    syntax_statement in progress

---
+++ syntax_statement/12345.py
@@ -0,0 +1,17 @@
+#1
+print("hello world")
+#2
+print("hi kbtu")
+print("hello world")
+#3
+a=2
+b=4
+print(a); print(b)
+#4
+a=4
+b=2
+print(a+b); print(a-b); print(a*b); print(a/b)
+#5
+name="mekhriniso"
+surname="darkhanova"
+print(f"Your name is {name} and"); print(f"your surname is {surname}, yes?")
