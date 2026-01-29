06f1d39c9ca8aef7b0c0d63d97d6c33d46bf6446
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-29 11:46:28 +0500

    if_statement in progress

---
+++ if_statement/12345.py
@@ -0,0 +1,22 @@
+#1
+a = 3
+if a>2:
+    print(a)
+#2
+a = 5
+b = 8
+if a==b:
+    print(f"{a} == {b}")
+#3
+a = 4
+if a%2!=0:
+    print(f"a={a} is odd")
+#4
+a = 10
+b = 2
+if a%b==0:
+    print(a%b)
+#5
+a = "apple"
+if len(a)>=3:
+    print(len(a))
