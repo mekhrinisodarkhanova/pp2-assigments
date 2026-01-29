e988c42a41ee6f83b88eca9416155376c4f3b692
mekhrinisodarkhanova <m_darkhanova@kbtu.kz>
2026-01-29 18:43:18 +0500

    if_else in progress

---
+++ if_else/12345.py
@@ -0,0 +1,35 @@
+#1
+a = 5
+if a%2==0:
+    print("even")
+else:
+    print("odd")
+#2
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+#3
+username = "Emil"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+#4
+a = 567
+b = 9865
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+#5
+a = 987752
+if a%2==0:
+    print("even")
+else:
+    print("odd")
